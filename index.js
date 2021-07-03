c5c3159f064508a5a4a87557d95b80de8d15017d
Muhammad Khalid Saifullah <khalidsaifullahmks@gmail.com>
2021-07-03 19:36:54 +0500

    This is my codding on java script

---
+++ index.js
@@ -0,0 +1,74 @@
+console.log("This is my practice on JS");
+var myName = "Khalid"
+var myName1 = "Saifullah"
+console.log(myName + myName1)
+console.log(myName, myName1)
+console.log(`This is my name= ${myName} ${myName1}`);
+let name = "my name is khalid";//for declearing simple variable
+const name1 = "my Name is muhammad khalid saifullah";
+console.log(`This is my name= ${name} `);//for declearing const variable
+const age = 20;
+const age1 = "20";
+if (age < 20) {
+    console.log("Age is less than 20");
+}
+else
+    console.log("Age is gerater than 20");
+
+age < 20 && console.log("age is less than 20");
+age < 20 ? console.log("you age is less than 20"):
+ console.log("your age is greater than 20")
+ if (age === age1) {
+    console.log("both ages equal");
+}
+else
+console.log("both ages are not equal")
+
+// console.log("hello! world");
+// // before ES6
+// var myVar = "var keyword"; //use ni krna
+
+// //after ES6
+// const myConst = "const keyword"; //pehly use krna hy
+// let myLet = "let keyword"; // agr value change type ki hy to use krna hy
+// console.log(myLet);
+
+// // myConst = "new value";
+// console.log(myConst);
+
+// myLet = "new value";
+// console.log(myLet);
+
+// let age = 20;
+
+// if (age > 18) {
+//   console.log("you are eligible");
+//   //   alert("you are eligible");
+// }
+
+//age > 18 && console.log("you are eligible");
+
+// age = 15;
+
+// if (age > 18) {
+//   console.log("you are eligible");
+// } else {
+//   console.log("you are not authorized");
+// }
+
+// age > 18
+//   ? console.log("you are eligible")
+//   : console.log("you are not authorized");
+
+// const myNum = 2 ** 4;
+// console.log(myNum);
+
+// let num1 = 2;
+// const num2 = "2";
+// console.log(typeof num1);
+// console.log(typeof num2);
+
+// if (num1 === num2) {
+//   console.log("equal");
+// } else {
+//   consol
